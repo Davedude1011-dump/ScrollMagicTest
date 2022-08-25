fd60db048985ca0ac37f43768fb0172152f82325
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-08-25 19:11:39 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,141 @@
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box1',
+    triggerHook: 0.3
+})
+.setClassToggle('.box1', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box2',
+    triggerHook: 0.3
+})
+.setClassToggle('.box2', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box3',
+    triggerHook: 0.3
+})
+.setClassToggle('.box3', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box4',
+    triggerHook: 0.3
+})
+.setClassToggle('.box4', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box5',
+    triggerHook: 0.3
+})
+.setClassToggle('.box5', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box6',
+    triggerHook: 0.35
+})
+.setClassToggle('.box6', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box7',
+    triggerHook: 0.35
+})
+.setClassToggle('.box7', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box8',
+    triggerHook: 0.35
+})
+.setClassToggle('.box8', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box9',
+    triggerHook: 0.35
+})
+.setClassToggle('.box9', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box10',
+    triggerHook: 0.35
+})
+.setClassToggle('.box10', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box11',
+    triggerHook: 0.3
+})
+.setClassToggle('.box11', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box12',
+    triggerHook: 0.3
+})
+.setClassToggle('.box12', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box13',
+    triggerHook: 0.3
+})
+.setClassToggle('.box13', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box14',
+    triggerHook: 0.3
+})
+.setClassToggle('.box14', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box15',
+    triggerHook: 0.3
+})
+.setClassToggle('.box15', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box16',
+    triggerHook: 0.35
+})
+.setClassToggle('.box16', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box17',
+    triggerHook: 0.35
+})
+.setClassToggle('.box17', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box18',
+    triggerHook: 0.35
+})
+.setClassToggle('.box18', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box19',
+    triggerHook: 0.35
+})
+.setClassToggle('.box19', 'close')
+.addTo(controller)
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '.box20',
+    triggerHook: 0.35
+})
+.setClassToggle('.box20', 'close')
+.addTo(controller)
